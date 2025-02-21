e6d1da40a14429027b7702e84d2f405bfd2ebc98
Enrique Davis <enriquedavis@gmail.com>
2025-02-21 19:53:12 +0100

    version 1.0

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -27,16 +27,32 @@ class HomeController extends Controller{
             ];
             //Contactos::create($datos);
             // Crear una nueva instancia del modelo y asignar valores
-            
+
+            // Daba un error que se solucionó colocando 
+            //  "public $timestamps = false;"
+            // en el modelo Contactos
+
             $nuevoContacto = new Contactos();
+            
             $nuevoContacto->nombre = $datos['nombre'];
             $nuevoContacto->telefono = $datos['telefono'];
-            $nuevoContacto->telefono = $datos['email'];
-            $nuevoContacto->telefono = $datos['direccion'];
+            $nuevoContacto->email = $datos['email'];
+            $nuevoContacto->direccion = $datos['direccion'];
+            
             $nuevoContacto->save(); // Guardar el registro en la base de datos
             
+            $this->index(); 
+            //OJO se ejecuta index de HomeController y 
+            // una vez hecho entonces vuelve a este punto.
+            // Por ello, si no se coloca el else se ejecutaría
+            // el view('newContacto'), lo que hace que se 
+            // muestre la vista de newContacto debajo de la 
+            // vista de home...
+            // Otra forma era usar return $this->index();
         }
-        $this->view("newContacto"); 
+        else{
+            $this->view('newContacto');
+        }
     }
 
     public function del($params){
@@ -45,19 +61,49 @@ class HomeController extends Controller{
         // Borrar el registro
         $contacto->delete();
 
-        $this->view('home', $data);
+        $this->index();   
     }
 
     public function edit($params){
-        // Encontrar el registro por su ID
+        
         $contacto = Contactos::find($params);
-        // Borrar el registro
-     
-
-        $this->view('home', $data);
+        $datos = [
+            'mensaje' => 'Datos del constacto a editar',
+            'contacto'=>$contacto
+        ];
+        
+        //if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        if (isset($_POST['nombreApellido'])) {
+            $this->update($params);
+        }
+    
+        $this->view('editarContacto', $datos);
     }
 
-
+    public function update($params){
+           
+        $contacto = Contactos::find($params);
+    
+        $contacto->nombre = $_POST['nombreApellido'];
+        $contacto->telefono = $_POST['tlf'];
+        $contacto->email = $_POST['email'];
+        $contacto->direccion = $_POST['direccion'];
+        $contacto->save();
+        
+        /*
+        $contacto = Contactos::find($params);
+    
+        $datos = [
+            'mensaje' => 'Editado',
+            'contacto'=>$contacto
+        ];
+      
+        $this->view('home', $datos);
+        */
+        // dado que se requiere volver a index, 
+        // lo anterior se puede sustituir por
+        $this->index();
+    }
 
 }
 ?>
--- app/views/editarContacto.php
+++ app/views/editarContacto.php
@@ -3,33 +3,49 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Vista de Datos</title>
+    <title>vista de Datos del usuario ID</title>
 </head>
 <body>
-    <form action="index.php" method="POST">
-        <label for="id">ID del Registro:</label>
-        <input type="text" id="id" name="id" required>
-        <button type="submit">Buscar</button>
-    </form>
+    <?php 
+    /*var_dump($data); 
+    echo "br";
+    echo "br";
+    echo "br************";
+    echo $data['contacto']->nombre;
+    exit();
+    */
+    ?>
     
-    <?php if (isset($mensaje)): ?>
-        <p><?php echo htmlspecialchars($mensaje); ?></p>
+    <?php if ((isset($data['mensaje'])) && ($data['mensaje'] != '')): ?>
+        <h2><?php echo htmlspecialchars($data['mensaje']); ?></h2>
     <?php endif; ?>
 
-    <?php if ($dato): ?>
-        <h2>Detalles del Registro</h2>
-        <form action="index.php" method="POST">
-            <input type="hidden" name="id" value="<?php echo htmlspecialchars($dato->id); ?>">
-            <label for="campo1">Campo1:</label>
-            <input type="text" id="campo1" name="campo1" value="<?php echo htmlspecialchars($dato->campo1); ?>" required><br><br>
+    <?php if ($data): ?>
+       
+        
+        <form action=<?php echo "http://127.0.0.1/agenda/home/update/" . $data['contacto']->id; ?> method="POST">
+            <input type="text" name="id" placeholder="<?php echo htmlspecialchars($data['contacto']->id); ?>" readonly><br><br>
             
-            <label for="campo2">Campo2:</label>
-            <input type="text" id="campo2" name="campo2" value="<?php echo htmlspecialchars($dato->campo2); ?>" required><br><br>
+            <label for="nombre">Nombre y Apellido:</label>
+            <input type="text" id="campo1" name="nombreApellido" placeholder="<?php echo htmlspecialchars($data['contacto']->nombre); ?>" required><br><br>
+            
+            <label for="campo2">Teléfono:</label>
+            <input type="text" id="campo2" name="tlf" placeholder="<?php echo htmlspecialchars($data['contacto']->telefono); ?>" required><br><br>
 
-            <button type="submit" name="update">Actualizar</button>
+            <label for="campo3">Email:</label>
+            <input type="text" id="campo3" name="email" placeholder="<?php echo htmlspecialchars($data['contacto']->email); ?>" required><br><br>
+
+            <label for="campo4">Dirección:</label>
+            <input type="text" id="campo4" name="direccion" placeholder="<?php echo htmlspecialchars($data['contacto']->direccion); ?>" ><br><br>
+
+            <button type="submit" name="upd">Actualizar</button>
         </form>
-    <?php elseif ($datoId): ?>
-        <p>Registro no encontrado.</p>
     <?php endif; ?>
+    <form action="http://127.0.0.1/agenda/" method="post">
+        <input type="text" name="cancelar" style="display: none">
+        <input type="submit" value="CANCELAR"><br>
+    </form>
+
 </body>
 </html>
+
